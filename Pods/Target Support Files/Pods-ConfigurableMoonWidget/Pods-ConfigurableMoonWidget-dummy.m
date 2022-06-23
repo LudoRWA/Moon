653f5bb3e4c0943e829c813e873b2b09abfdf98c
Ludo <ludovic.roullierdev@gmail.com>
2022-06-23 11:28:31 +0200

    Clean

--- Pods/Target Support Files/Pods-ConfigurableMoonWidget/Pods-ConfigurableMoonWidget-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ConfigurableMoonWidget : NSObject
-@end
-@implementation PodsDummy_Pods_ConfigurableMoonWidget
-@end
--- Pods/Target Support Files/Pods-MoonWidgetExtension/Pods-MoonWidgetExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MoonWidgetExtension : NSObject
-@end
-@implementation PodsDummy_Pods_MoonWidgetExtension
-@end
